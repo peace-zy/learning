c8dc9bf3b17f3d30b580f97758aa6a3c7e0d5b5d
peace-zy <260259423@qq.com>
2023-01-03 15:27:40 +0800

    Update paddl2_to_onnx.sh

--- paddle/paddl2_to_onnx.sh
+++ paddle/paddl2_to_onnx.sh
@@ -1,5 +1,10 @@
-paddle2onnx --model_dir conv_nhwc_to_nchw \
+inpath="modified_network/conv_nhwc_to_nchw"
+save_path=${inpath}/in_onnx
+if [ ! -d ${save_path} ];then
+  mkdir ${save_path}
+fi
+paddle2onnx --model_dir modified_network/conv_nhwc_to_nchw \
             --model_filename __model__ \
             --params_filename __params__ \
-            --save_file online_clip_model_20221227.onnx \
+            --save_file ${save_path}/online_clip_model.onnx \
             --enable_dev_version True
