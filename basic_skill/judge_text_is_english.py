59794dc017eddf24a4dfbcb4acd833359d69217f
shaoping <260259423@qq.com>
2024-05-13 18:02:07 +0800

    Update judge_text_is_english.py

--- basic_skill/judge_text_is_english.py
+++ basic_skill/judge_text_is_english.py
@@ -1,4 +1,4 @@
 # 判断文本是否是英文，去除标点和空格后，帮段是否是alpha
 import string
 translator = str.maketrans('', '', string.punctuation + string.whitespace)
-question.translate(translator).isalpha()
+question.translate(translator).encode("utf-8").isalpha()
